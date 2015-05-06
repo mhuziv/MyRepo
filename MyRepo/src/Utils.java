407b527830324cd682663863e0808a38ee7325f0
mhuziv <mhusiv@ksf.kiev.ua>
2015-05-06 16:41:11 +0300

    Add utils

---
+++ MyRepo/src/Utils.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+
+/**
+ *
+ * @author mhuziv
+ */
+public class Utils {
+    
+    public static void ShowMessage() {
+        System.out.println("message");
+    }
+    
+}
